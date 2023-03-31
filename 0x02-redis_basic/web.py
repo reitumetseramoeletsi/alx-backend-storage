fe136a85a1a61e851b5f335a254e48960366634f
Reitumetse <joycreamoeletsi@gmail.com>
2023-03-31 14:23:28 +0200

    update files

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-""" Implementing an expiring web cache and tracker
-    obtain the HTML content of a particular URL and returns it """
+"""
+Create a web cach
+"""
 import redis
 import requests
-r = redis.Redis()
+rc = redis.Redis()
 count = 0
 
 
 def get_page(url: str) -> str:
-    """ track how many times a particular URL was accessed in the key
-        "count:{url}"
-        and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
     resp = requests.get(url)
-    r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
     return resp.text
 
 
